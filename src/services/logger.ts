baa272375e18fdb22dad2e67382b9cf76a6e8561
Shirshendu Bhowmick <shirshendu.bhowmick3@gmail.com>
2021-10-13 21:03:15 +0530

    added crude logger

---
+++ src/services/logger.ts
@@ -0,0 +1,23 @@
+class Logger {
+  logger: Console;
+
+  constructor() {
+    this.logger = console;
+  }
+
+  logInfo(message: string, payload: object) {
+    this.logger.log({ level: 'INFO', message, ...payload });
+  }
+
+  logError(message: string, payload: object) {
+    this.logger.log({ level: 'ERROR', message, ...payload });
+  }
+
+  logDebug(message: string, payload: object) {
+    this.logger.log({ level: 'DEBUG', message, ...payload });
+  }
+}
+
+const logger = new Logger();
+
+export default logger;
