f18c3ece4c50c5bace8782f9dc18a57983a8c362
Shirshendu Bhowmick <shirshendu.bhowmick3@gmail.com>
2021-10-13 20:08:50 +0530

    added user map

---
+++ src/constants/userMap.ts
@@ -0,0 +1,24 @@
+const userMap = {
+  avtr001: {
+    userId: 'avtr001',
+    name: 'John',
+  },
+  avtr002: {
+    userId: 'avtr002',
+    name: 'Jane',
+  },
+  avtr003: {
+    userId: 'avtr003',
+    name: 'Joe',
+  },
+  avtr004: {
+    userId: 'avtr004',
+    name: 'Tom',
+  },
+  avtr005: {
+    userId: 'avtr005',
+    name: 'Harry ',
+  },
+};
+
+export default userMap;
